45756d5cbfa3cf54a0ec805ef87e020a1bdd484b
Deepak vadivel <v.deepakvadivel@gmail.com>
2024-03-21 10:14:46 +0530

    first commit

---
+++ css/toggle.js
@@ -0,0 +1,5 @@
+// const toggle = document.querySelector('.toggle1');
+// const navbar = document.querySelector('ul');
+// toggle.addEventListener('click',()=>{
+//     navbar.classList.toggle('slide')
+// });
